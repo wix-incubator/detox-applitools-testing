b3c8a4894453da8edd267f34f2568be952bc37ef
Gytis Vinclovas <gytis.vinclovas@gmail.com>
2019-11-04 14:42:04 +0200

    Using detox new screenshot api.

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-const {execSync} = require('child_process');
-const {existsSync} = require('fs');
 const path = require('path');
-const screenshotUtilsFactory = require('./screenshots');
 const packageJSON = require(path.resolve(process.cwd(), './package.json'));
 const argparse = require('detox/src/utils/argparse');
 
@@ -9,12 +6,9 @@ module.exports = {
   _testDisabled: false,
   _eyes: null,
   _config: null,
-  _screenshotPath: null,
-  _screenshotUtils: null,
 
   setup: (config) => {
     if (!config) {
-
       throw new Error('Detox applitools configuration is missing! \n Necessary configuration params: batchId, apiKey, appName, serverUrl');
     }
 
@@ -33,15 +27,6 @@ module.exports = {
     const configurationName = argparse.getArgValue('configuration');
     const detox = packageJSON.detox.configurations[configurationName];
     this._eyes = require('./eyes')({batchId, appName, apiKey, serverUrl, hostOS: detox.type, hostApp: detox.name || (typeof detox.device === 'string' ? detox.device : detox.device.name)});
-
-    this._screenshotPath = execSync('mktemp -t dat -d').toString().trim();
-    this._screenshotUtils = screenshotUtilsFactory(this._screenshotPath);
-  },
-
-  cleanup: () => {
-    if (existsSync(this._screenshotPath)) {
-      execSync('rm -rf ' + this._screenshotPath);
-    }
   },
 
   testScreenshot: async (id, options = {}) => {
@@ -49,10 +34,11 @@ module.exports = {
       return;
     }
 
-    await this._screenshotUtils.takeStoryScreenshot(id);
+    const screenshotPath = await device.takeScreenshot(id);
+
     await this._eyes.open(this._config.appName || 'APP_NAME_NOT_SET', id);
 
-    await this._eyes.checkRegion(this._screenshotUtils.getScreenshot(id), {
+    await this._eyes.checkRegion(screenshotPath, {
       left: 0,
       top: options.ignoredTopHeight === undefined ? 44 : options.ignoredTopHeight,
       width: 5000,

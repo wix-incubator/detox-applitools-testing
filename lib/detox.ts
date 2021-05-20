bfd2fa10b093079297be5dcf30b5d9b832b26a47
Emilis Baliukonis <emilisb@wix.com>
2021-05-20 15:44:10 +0300

    Do not use Detox config file

--- lib/detox.ts
+++ lib/detox.ts
@@ -1,16 +1,12 @@
-import * as path from 'path';
 import {device} from 'detox';
-import * as argparse from 'detox/src/utils/argparse';
 import {statusBarHeights, defaultHeight} from './status-bar';
 
 export const getDeviceType = (): string => {
-  const config = getDetoxConfig();
-  return config.type;
+  return device.getPlatform();
 };
 
 export const getDeviceName = (): string => {
-  const {name, device} = getDetoxConfig();
-  return name || (typeof device === 'string' ? device : device.name);
+  return device.name;
 };
 
 export const getStatusBarHeight = (): number => {
@@ -21,11 +17,4 @@ export const getStatusBarHeight = (): number => {
 
 export const takeScreenshot = (name: string): Promise<string> => {
   return (device.takeScreenshot(name) as unknown) as Promise<string>;
-};
-
-const getDetoxConfig = () => {
-  // eslint-disable-next-line @typescript-eslint/no-var-requires
-  const packageJSON = require(path.resolve(process.cwd(), './package.json'));
-  const configurationName = argparse.getArgValue('configuration');
-  return packageJSON.detox.configurations[configurationName];
 };
